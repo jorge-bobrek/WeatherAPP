451fbf29185f019c96ed15f2f68d49d02cda38de
Jorge Bobrek <jrbobrek@gmail.com>
2025-01-28 13:21:13 -0500

    Initial Commit

---
+++ WeatherAPI/WeatherAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAPIApp.swift
+//  WeatherAPI
+//
+//  Created by Jorge Bobrek on 28/01/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherAPITests/WeatherAPITests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAPITests.swift
+//  WeatherAPITests
+//
+//  Created by Jorge Bobrek on 28/01/25.
+//
+
+import Testing
+@testable import WeatherAPI
+
+struct WeatherAPITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
